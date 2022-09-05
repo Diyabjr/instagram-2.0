fe3f0530eea2cefb63fac06d6c2ce736822208b2
Diab Alshrk <diyabjr@icloud.com>
2022-09-05 12:24:16 +0200

    SAP

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,38 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  secret: "smece",
+
+  theme: {
+      logo: "https://links.papareact.com/sq0",
+      brandColor: "#F13287",
+      colorScheme: "auto",
+  },
+  pages:{
+      signIn: "/auth/signin",
+  },
+  callbacks: {
+    async session({session, token, user}) {
+
+      session.user.username = session.user.name
+      .split(' ')
+      .join('')
+      .toLocaleLowerCase();
+
+      // Lucija Karan (name attribute)
+      // lucijakaran (username)
+
+      session.user.uid = token.sub;
+      return session;
+    }
+  }
+});
